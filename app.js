70e486dced6067bfcf05b59fa64fcdd969f0cf31
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@vitstudent.ac.in>
2023-08-26 10:27:08 +0530

    Added Passport Configuration

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const expressLayouts = require('express-ejs-layouts');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 
 const connectDB = require('./server/config/db.js');
 
 const app = express();
 const port = 5000 || process.env.PORT;
+
+// User Model (replace with actual user Model created)
+const User = require('./server/models/User');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //allows to use form values
@@ -34,6 +40,42 @@ app.use(session({
 app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
+
+// Passport Configuration
+passport.use(new LocalStrategy(
+    {usernameField: 'email' },
+    async(email, password, done) => {
+        try{
+            const user = await User.findOne({ email });
+
+            if(!user){
+                return done(null, false, { message: 'Incorrect email or password' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(isMatch){
+                return done(null, user);
+            } else{
+                return done(null, false, { message: 'Incorrect email or password' });
+            }
+        } catch(err){
+            return done(err);
+        }
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async(id, done)=>{
+    try{
+        const user = await User.findById(id);
+        done (null, user);
+    } catch(err){
+        done(err);
+    }
+});
 
 app.use('/', require('./server/routes/main'))
 app.use('/', require('./server/routes/admin'))
